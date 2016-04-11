c660986afafcf507db92d1864845a25030c7dfff
dragonslayer0531 <nicholaus.clark115@gmail.com>
2016-04-11 16:26:13 -0400

    Added Documentation

--- src/luminoscore/audio/AudioMaster.java
+++ src/luminoscore/audio/AudioMaster.java
@@ -7,18 +7,36 @@ import org.lwjgl.openal.AL10;
 import org.lwjgl.openal.ALC;
 import org.lwjgl.util.WaveData;
 
+/**
+ * 
+ * @author Nick Clark
+ * @version 1.0
+ * 
+ * Creates audio master
+ *
+ */
+
 public class AudioMaster {
 	
 	private List<Integer> buffers = new ArrayList<Integer>();
-		
+	
+	/**
+	 * Constructor
+	 */
 	public AudioMaster() {
 		
 	}
 	
+	/**
+	 * Initializes OpenAL Context
+	 */
 	public void init() {
 		ALC.create();
 	}
 	
+	/**
+	 * Deletes audio buffers and closes OpenAL Context
+	 */
 	public void dispose() {
 		for(Integer buffer : buffers) {
 			AL10.alDeleteBuffers(buffer);
@@ -26,6 +44,12 @@ public class AudioMaster {
 		ALC.destroy();
 	}
 	
+	/**
+	 * @param source	String containing data of source
+	 * @return int		Integer referencing index of Sound Card
+	 * 
+	 * Loads source to sound card
+	 */
 	public int loadSource(String source) {
 		int buffer = AL10.alGenBuffers();
 		buffers.add(buffer);
@@ -37,10 +61,24 @@ public class AudioMaster {
 		return buffer;
 	}
 	
+	/**
+	 * @param x		X Location of listening device
+	 * @param y		Y Location of listening device
+	 * @param z		Z Location of listening device
+	 * 
+	 * Sets listener position
+	 */
 	public void setListenerPosition(float x, float y, float z) {
 		AL10.alListener3f(AL10.AL_POSITION, x, y, z);
 	}
 	
+	/**
+	 * @param dx	X Movement of listening device
+	 * @param dy	Y Movement of listening device
+	 * @param dz	Z Movement of listening device
+	 * 
+	 * Sets listener velocity
+	 */
 	public void setListenerVelocity(float dx, float dy, float dz) {
 		AL10.alListener3f(AL10.AL_VELOCITY, dx, dy, dz);
 	}
--- src/luminoscore/audio/AudioSource.java
+++ src/luminoscore/audio/AudioSource.java
@@ -2,57 +2,120 @@ package luminoscore.audio;
 
 import org.lwjgl.openal.AL10;
 
+/**
+ * 
+ * @author Nick Clark
+ * @version 1.0
+ * 
+ * Audio source holder
+ *
+ */
+
 public class AudioSource {
 
 	private int sourceID;
 	
+	/**
+	 * Constructor
+	 */
 	public AudioSource() {
 		sourceID = AL10.alGenSources();
 	}
 	
+	/**
+	 * Deletes audio source
+	 */
 	public void dispose() {
 		stop();
 		AL10.alDeleteSources(sourceID);
 	}
 	
+	/**
+	 * @param buffer	Buffer to play
+	 * 
+	 * Plays source
+	 */
 	public void play(int buffer) {
 		stop();
 		AL10.alSourcei(sourceID, AL10.AL_BUFFER, buffer);
 		AL10.alSourcePlay(sourceID);
 	}
 	
+	/**
+	 * @param gain	Volume
+	 * 
+	 * Sets volume of source
+	 */
 	public void setGain(float gain) {
 		AL10.alSourcef(sourceID, AL10.AL_GAIN, gain);
 	}
 	
+	/**
+	 * @param pitch		Pitch
+	 * 
+	 * Sets pitch of source
+	 */
 	public void setPitch(float pitch) {
 		AL10.alSourcef(sourceID, AL10.AL_PITCH, pitch);
 	}
 	
+	/**
+	 * @param x		X location of source
+	 * @param y		Y location of source
+	 * @param z		Z location of source
+	 * 
+	 * Sets source location
+	 */
 	public void setPosition(float x, float y, float z) {
 		AL10.alSource3f(sourceID, AL10.AL_POSITION, x, y, z);
 	}
 	
+	/**
+	 * @param dx	X movement of source
+	 * @param dy	Y movement of source
+	 * @param dz	Z movement of source
+	 * 
+	 * Sets source velocity
+	 */
 	public void setVelocity(float dx, float dy, float dz) {
 		AL10.alSource3f(sourceID, AL10.AL_VELOCITY, dx, dy, dz);
 	}
 	
+	/**
+	 * @param loop		Repeat
+	 * 
+	 * Sets if source loops
+	 */
 	public void setLooping(boolean loop) {
 		AL10.alSourcei(sourceID, AL10.AL_LOOPING, loop ? AL10.AL_TRUE : AL10.AL_FALSE);
 	}
 	
+	/**
+	 * @return boolean	Active source
+	 * 
+	 * Gets if source is playing
+	 */
 	public boolean isPlaying() {
 		return AL10.alGetSourcei(sourceID, AL10.AL_SOURCE_STATE) == AL10.AL_PLAYING;
 	}
 	
+	/**
+	 * Pauses source
+	 */
 	public void pause() {
 		AL10.alSourcePause(sourceID);
 	}
 	
+	/**
+	 * Plays source from pause point
+	 */
 	public void continuePlay() {
 		AL10.alSourcePlay(sourceID);
 	}
 	
+	/**
+	 * Stops source
+	 */
 	public void stop() {
 		AL10.alSourceStop(sourceID);
 	}
